2e8e00fdb0fc83aa4f6ec9f6b3f949dafac6921b
Gautam Jain <rpgautam.jain@gmail.com>
2025-01-14 09:56:16 +0530

    Added a working stream class!!

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
-var mySymbol;
+// var mySymbol;
 const symbolSize=50;
+var stream;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(0);
-  mySymbol=new RedPill(
-    width/2,
-    0,
-    random(1,5));
-  mySymbol.setValueRandom();
+  
+//   mySymbol=new RedPill(
+//     width/2,
+//     0,
+//     random(1,5));
+//   mySymbol.setValueRandom();
+  stream=new Stream();
+  stream.generateSymbols();
   textSize(symbolSize);
 }
 
 function draw() {
   background(0);
-  mySymbol.renderMatrix();
+//   mySymbol.renderMatrix();
+  stream.renderingMatrix();
 }
 
 class RedPill{
@@ -40,25 +45,62 @@ class RedPill{
     
   }
   
-  renderMatrix(){
-    fill(0,255,70);
-    text(this.value,this.x,this.y)
-    this.rain();
-    this.setValueRandom();
+//   renderMatrix(){
+//     fill(0,255,70);
+//     text(this.value,this.x,this.y)
+//     this.rain();
+//     this.setValueRandom();
     
-  }
+//   }
   
   rain(){
       // if(this.y>=height){
       //   this.y=0;
       // }
       // this.y+=this.speed;
-      
+
       //we in da big leagues now
       this.y=(this.y>=height)? 0: this.y+=this.speed;
   }
-  
-  
+    
   
   
 }
+
+class Stream{
+    constructor(){
+        this.symbols=[];
+        this.totalSymbols=round(random(5,30));
+        this.speed=random(5,20);
+    }
+
+    generateSymbols(){
+        var y=0;
+        var x=width/2;
+
+        for(var i=0;i<=this.totalSymbols;i++){
+            var symbol=new RedPill(x,y,this.speed);
+            symbol.setValueRandom();
+            this.symbols.push(symbol);
+            //we want to decrement y by the symbol size to sort of set the next symbol directly above it instead of the the same place, theyre stacked
+            y-=symbolSize;
+
+        }
+
+    }
+
+
+    //there is more than 1 way to skin a cat 
+    renderingMatrix(){
+        this.symbols.forEach((symbol)=>{
+            fill(0,255,70);
+            text(symbol.value,symbol.x,symbol.y)
+            symbol.rain();
+            symbol.setValueRandom();
+            
+        });
+    }
+
+}
+
+
